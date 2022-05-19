69149858111c604aa2405b76f79d690ab0989d20
Juliano Zen <100943347+zenjulianozen@users.noreply.github.com>
2022-05-19 16:42:51 -0300

    Update HR_MULTIPLE_TABLES_QUERIES.sql

--- HR_MULTIPLE_TABLES_QUERIES.sql
+++ HR_MULTIPLE_TABLES_QUERIES.sql
@@ -33,3 +33,33 @@ WHERE JOB_IDENT IN (SELECT JOB_ID FROM EMPLOYEES WHERE YEAR(B_DATE) > 1976);
 SELECT *
 FROM JOBS
 WHERE JOB_IDENT IN (SELECT JOB_ID FROM EMPLOYEES WHERE YEAR(B_DATE) > 1976 AND SEX = 'F');
+
+--- Perform an implicit cartesian/cross join between EMPLOYEES and JOBS tables.
+
+SELECT *
+FROM EMPLOYEES, JOBS;
+
+--- Retrieve only the EMPLOYEES records that correspond to jobs in the JOBS table.
+
+SELECT *
+FROM EMPLOYEES, JOBS
+WHERE EMPLOYEES.JOB_ID = JOBS.JOB_IDENT;
+
+--- Redo the previous query, using shorter aliases for table names.
+
+SELECT *
+FROM EMPLOYEES E, JOBS J
+WHERE E.JOB_ID = J.JOB_IDENT;
+
+--- Redo the previous query, but retrieve only the Employee ID, Employee Name and Job Title.
+
+SELECT EMP_ID, F_NAME, L_NAME, JOB_TITLE
+FROM EMPLOYEES E, JOBS J
+WHERE E.JOB_ID = J.JOB_IDENT;
+
+--- Redo the previous query, but specify the fully qualified column names with aliases in 
+--- the SELECT clause.
+
+SELECT E.EMP_ID, E.F_NAME, E.L_NAME, J.JOB_TITLE 
+FROM EMPLOYEES E, JOBS J 
+WHERE E.JOB_ID = J.JOB_IDENT;
