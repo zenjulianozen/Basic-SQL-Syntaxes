04e493deae196d9bb763e9fd3800c49998018ef1
Juliano Zen <100943347+zenjulianozen@users.noreply.github.com>
2022-05-19 15:28:18 -0300

    Create HR_NESTED_QUERIES.sql

---
+++ HR_NESTED_QUERIES.sql
@@ -0,0 +1,31 @@
+--- Execute a failing query (i.e. one which gives an error, yes, really) to retrieve all 
+--- employees records whose salary is lower than the average salary.
+
+SELECT F_NAME, L_NAME
+FROM EMPLOYEES
+WHERE SALARY < AVG(SALARY);
+
+--- Execute a working query using a sub-select to retrieve all employees records whose 
+--- salary is lower than the average salary.
+
+SELECT F_NAME, L_NAME
+FROM EMPLOYEES
+WHERE SALARY < (SELECT AVG(SALARY) FROM EMPLOYEES);
+
+--- Execute a failing query (i.e. one which gives an error) to retrieve all employees 
+--- records with EMP_ID, SALARY and maximum salary as MAX_SALARY in every row.
+
+SELECT EMP_ID, SALARY, MAX(SALARY) AS MAX_SALARY
+FROM EMPLOYEES;
+
+--- Execute a Column Expression that retrieves all employees records with EMP_ID, SALARY 
+--- and maximum salary as MAX_SALARY in every row.
+
+SELECT EMP_ID, SALARY, (SELECT MAX(SALARY) FROM EMPLOYEES) AS MAX_SALARY
+FROM EMPLOYEES;
+
+--- Execute a Table Expression for the EMPLOYEES table that excludes columns with sensitive 
+--- employee data (i.e. does not include columns: SSN, B_DATE, SEX, ADDRESS, SALARY).
+
+SELECT * 
+FROM (SELECT EMP_ID, F_NAME, L_NAME, DEP_ID FROM EMPLOYEES) AS EMPLOYEE_HIDE
